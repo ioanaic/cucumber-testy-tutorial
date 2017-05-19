1dd8ab8bf0a10219c793b2d54f83effdbbc488c7
ioanaic <crisan.ioana27@gmail.com>
2017-05-19 14:52:16 +0300

    Commit LoginTest

--- src/test/java/org/fasttrackit/automation/LoginTest.java
+++ src/test/java/org/fasttrackit/automation/LoginTest.java
@@ -1,10 +1,9 @@
 package org.fasttrackit.automation;
 
-import com.sdl.selenium.web.utils.Utils;
 import org.fasttrackit.util.TestBase;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
-import org.testng.Assert;
+import org.openqa.selenium.support.PageFactory;
 import org.testng.annotations.Test;
 
 import static org.hamcrest.CoreMatchers.is;
@@ -12,40 +11,31 @@ import static org.hamcrest.MatcherAssert.assertThat;
 
 public class LoginTest extends TestBase {
 
+    private LoginPage page;
+
+    public LoginTest() {
+        page = PageFactory.initElements(driver, LoginPage.class);
+    }
+
     @Test
     public void validLoginTest() {
         doLogin("eu@fast.com", "eu.pass");
-        WebElement logout = driver.findElement(By.xpath("//a[@class='btn btn-default navbar-btn']"));
+        WebElement logout = driver.findElement(By.linkText("Logout"));
         logout.click();
-        ;
 
     }
 
     @Test
     public void invalidUserAndPasswordTest() {
-        driver.get("https://rawgit.com/sdl/Testy/master/src/test/functional/app-demo/login.html");
+        doLogin("wrong.user", "wrong.pass");
 
-        WebElement invalidUser = driver.findElement(By.name("username"));
-        invalidUser.sendKeys("eu.pasddskjghf");
-        WebElement invalidPassword = driver.findElement(By.id("password"));
-        invalidPassword.sendKeys("eu.paskjfh");
-        WebElement loginButton = driver.findElement(By.id("loginButton"));
-        loginButton.click();
-
-        doLogin("eu.pasddskjghf", "");
-
-
-        WebElement errorMsg = driver.findElement(By.className("error-msg"));
-        String message = errorMsg.getText();
+        String message = page.getInvalidUserOrPassWarningMessage();
         System.out.println(message);
 
         assertThat(message, is("Invalid user or password!"));
 
 
     }
-
-
-
 
 
 }
