72220a2cec06d5fbee5e2843a9872981f56ab2c1
FirstJavaProj <crisan_ioana2005@yahoo.com>
2017-05-25 19:22:38 +0300

    test

---
+++ src/main/java/org/fasttrackit/automation/LoginView.java
@@ -0,0 +1,21 @@
+package org.fasttrackit.automation;
+
+
+import com.sdl.selenium.web.WebLocator;
+import com.sdl.selenium.web.form.TextField;
+
+/**
+ * Created by Ioana on 5/25/2017.
+ */
+public class LoginView {
+    public TextField userNameElement = new TextField().setName("username").setId("email");
+    public TextField passwordElement = new TextField().setId("password");
+    public WebLocator loginBtn = new WebLocator().setId("loginButton");
+
+    public void login(String user, String pass){
+        userNameElement.setValue(user);
+        passwordElement.setValue(pass);
+        loginBtn.click();
+    }
+
+}
